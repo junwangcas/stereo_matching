c003b1376df5701113838ac3a5762c8fa574199d
Jun Wang <wangjun@pfdm.com>
2022-10-10 20:24:12 +0800

    Update CMakeLists.txt, Untitled.m, and 3 more files...

---
+++ sourishg_stereo_calibration/python/trans_xml.py
@@ -0,0 +1,59 @@
+import xml.etree.ElementTree as ET
+
+filexml = '/sdcard/test_case/SLAMRecord/helmet_2022-08-01-15-11-58/device_calibration.xml'
+
+
+def PrintK(focal_length, principal_point):
+    print("K matrix: ")
+    focal_lengths = focal_length.split(' ')
+    principal_points = principal_point.split(' ')
+    print(focal_lengths[0], ', ', 0.0, ', ', principal_points[0], ', ', 0.0, ', ', focal_lengths[1], ', ', principal_points[1], ', ', 0.0, ', ', 0.0, ', ', 1)
+
+def PrintP(focal_length, principal_point, translation):
+    print("P matrix: ")
+    focal_lengths = focal_length.split(' ')
+    principal_points = principal_point.split(' ')
+    translations = translation.split(' ')
+    print(focal_lengths[0], ', ', 0.0, ', ', principal_points[0], ', ', translations[0], ', ', 0.0, ', ', focal_lengths[1], ', ', principal_points[1], ', ',translations[1], ', ', 0.0, ', ', 0.0, ', ', 1, ', ', translations[2])
+
+def PrintRadioD(radial_distortion):
+    print("radial_distortion: ")
+    radial_distortions = radial_distortion.split(' ')
+    print(radial_distortions[0], ', ', radial_distortions[1], ', ', radial_distortions[2], ', ', radial_distortions[3])
+
+def PrintR(rowMajorRotationMat):
+    print('RotationMatrix: ')
+    rowMajorRotationMats = rowMajorRotationMat.split(' ')
+    r_str = ''
+    for ele in rowMajorRotationMats:
+        r_str = r_str + ele + ', '
+    print(r_str)
+
+
+root = ET.parse(filexml).getroot()
+for Camera in root.findall('Camera'):
+    id = int(Camera.get('id'))
+    if (id != 0 and id != 3):
+        continue
+    print('cam id ', id)
+    Calibration = Camera.find("Calibration")
+    principal_point = Calibration.get("principal_point")
+    # print(principal_point)
+    focal_length = Calibration.get("focal_length")
+    # print(focal_length)
+    radial_distortion = Calibration.get("radial_distortion")
+    PrintRadioD(radial_distortion)
+    PrintK(focal_length, principal_point)
+
+    Rig = Camera.find("Rig")
+    translation = Rig.get("translation")
+    # print(translation)
+    rowMajorRotationMat = Rig.get("rowMajorRotationMat")
+    # print('RotationMatrix ', rowMajorRotationMat)
+    PrintR(rowMajorRotationMat)
+    PrintP(focal_length, principal_point, translation)
+    print("--------------------------------------------------")
+
+
+
+
--- sourishg_stereo_calibration/undistort_rectify_fisheye.cpp
+++ sourishg_stereo_calibration/undistort_rectify_fisheye.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <iostream>
 #include "popt_pp.h"
+// reference: https://sourishghosh.com/2016/stereo-calibration-cpp-opencv/
 
 using namespace std;
 using namespace cv;
@@ -35,8 +36,8 @@ int main(int argc, char const *argv[])
   Mat K1, K2, R;
   Vec3d T;
   Mat D1, D2;
-  Mat img1 = imread(leftimg_filename, CV_LOAD_IMAGE_COLOR);
-  Mat img2 = imread(rightimg_filename, CV_LOAD_IMAGE_COLOR);
+  Mat img1 = imread(leftimg_filename, CV_LOAD_IMAGE_GRAYSCALE);
+  Mat img2 = imread(rightimg_filename, CV_LOAD_IMAGE_GRAYSCALE);
 
   cv::FileStorage fs1(calib_file, cv::FileStorage::READ);
   fs1["K1"] >> K1;
@@ -56,9 +57,11 @@ int main(int argc, char const *argv[])
   cv::Mat imgU1, imgU2;
 
   // K1 - 内参， D1 - 畸变参数, R1 - 基本是一个单位矩阵，P1 - 貌似是一个K阵和T
-  cv::initUndistortRectifyMap(K1, D1, R1, P1, img1.size(), CV_32F, lmapx, lmapy);
+  cv::fisheye::initUndistortRectifyMap(K1, D1, R1, P1, img1.size(), CV_32F, lmapx, lmapy);
+//  cv::initUndistortRectifyMap(K1, D1, R1, P1, img1.size(), CV_32F, lmapx, lmapy);
   //
-  cv::initUndistortRectifyMap(K2, D2, R2, P2, img2.size(), CV_32F, rmapx, rmapy);
+  cv::fisheye::initUndistortRectifyMap(K2, D2, R2, P2, img2.size(), CV_32F, rmapx, rmapy);
+//  cv::initUndistortRectifyMap(K2, D2, R2, P2, img2.size(), CV_32F, rmapx, rmapy);
   cv::remap(img1, imgU1, lmapx, lmapy, cv::INTER_LINEAR);
   cv::remap(img2, imgU2, rmapx, rmapy, cv::INTER_LINEAR);
 
